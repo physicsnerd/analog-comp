98543315be1cdda651200be668fb7d52da319a4d
Unknown <heather.nerd@gmail.com>
2018-07-10 11:09:58 -0700

    added subtract() testing

--- testing/basicfunctions_unit_test.py
+++ testing/basicfunctions_unit_test.py
@@ -6,11 +6,10 @@ def add_tests_correct():
     long = b.add(2, 3, 5, 2, 2)
     float_add = b.add(1.2, 3.4)
     return [basic, long, float_add]
-    return [error_short, error_long, error_string, error_list]
 
 assert add_tests_correct() == [4, 14, 4.6]
 
-errors = {'short':'n', 'long':'n', 'string':'n', 'list':'n', 'mixed':'n'}
+errors = {'short':'n', 'long':'n', 'string':'n', 'list':'n'}
 
 try:
     b.add(1)
@@ -32,10 +31,37 @@ try:
 except:
     errors['list'] = 'y'
 
-try:
-    b.add(1, [2], 'c')
-except:
-    errors['mixed'] = 'y'
-
 print(errors)
 
+#b.subtract tests
+def subtract_tests_correct():
+    basic = b.subtract(2, 2)
+    float_sub = b.subtract(2.2, 1.2)
+    negative = b.subtract(2, 3)
+    return [basic, float_sub, negative]
+
+assert subtract_tests_correct() == [0, 1, -1]
+
+errors = {'short':'n', 'long':'n', 'string':'n', 'list':'n'}
+
+try:
+    b.subtract(1)
+except:
+    errors['short'] = 'y'
+
+try:
+    b.subtract(1, 2, 3)
+except:
+    errors['long'] = 'y'
+
+try:
+    b.subtract('hi', 'h')
+except:
+    errors['string'] = 'y'
+
+try:
+    b.subtract([1, 2], [1])
+except:
+    errors['list'] = 'y'
+
+#b.multiply tests
