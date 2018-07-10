a3565390603193b30e97c9ed9edf5166f7f75e7e
Unknown <heather.nerd@gmail.com>
2018-07-10 09:34:25 -0700

    updated to analogpack (integrate() and time_handle()) and ran pylint
    again, didn't change constants to UPPER_CASE style.

--- simulations/vanderpolsim.py
+++ simulations/vanderpolsim.py
@@ -1,5 +1,5 @@
-import basicfunctions as b
 import matplotlib.pyplot as plt
+from analogpack import basicfunctions as b
 
 mu = 1
 
@@ -22,7 +22,7 @@ max_steps = 4000
 method = 'simp38'
 
 while step <= max_steps:
-    time.append(t_step*step)
+    #time.append(t_step*step)
 
     if step == 0:
         v_pos = v_init
@@ -31,18 +31,23 @@ while step <= max_steps:
         x_array.append(x_pos)
         v_array.append(v_pos)
 
+        time.append(0)
         signal_array.append(signal)
     else:
+        time_vals = b.time_handle(step, t_step, time, method)
+        time = time_vals[0]
+        t_step = time_vals[1]
+
         signal = (mu*(1-x_pos**2))*v_pos + x_pos
         signal_array.append(signal)
 
         v_pos = b.integrate(step, t_step, signal_array, v_pos, method, v_init)
         v_array.append(v_pos)
-        
+
         x_pos = b.integrate(step, t_step, v_array, x_pos, method, x_init)
         x_array.append(x_pos)
 
-    step+=1
+    step += 1
 
 plt.plot(time, x_array)
 plt.show()
