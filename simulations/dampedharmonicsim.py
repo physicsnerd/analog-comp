12be98ffe766d9f1196732c29dc64133c489ea86
Unknown <heather.nerd@gmail.com>
2018-07-10 09:53:53 -0700

    updated to use time_handle() and analogpack, also ran through pylint
    with pylint, didn't change constants to UPPER_CASE and didn't fix a couple of lines that were too long.
    
    not 100% sure result is correct, but it looks right.

--- simulations/dampedharmonicsim.py
+++ simulations/dampedharmonicsim.py
@@ -1,20 +1,18 @@
-import matplotlib.pyplot as plt
 import math
-from tabulate import tabulate
+import matplotlib.pyplot as plt
+#from tabulate import tabulate
 from analogpack import basicfunctions as b
 
 k = 78.125
 m = 78.125
 c = 7
 omega = b.sqroot(b.divide(k, m))
-zeta = b.divide(c, b.multiply(2, b.sqroot(b.multiply(m,k))))
-
+zeta = b.divide(c, b.multiply(2, b.sqroot(b.multiply(m, k))))
 
 #omega = 1
 #zeta = 1
 
-#methods = ['trap','simp','simp38','boole','5th']
-methods = ['simp38']
+methods = ['trap', 'simp13', 'timevary']
 
 results = {}
 
@@ -39,59 +37,50 @@ for i in methods:
     velocity_val = 0
     time = []
     while step <= max_steps:
-        #time.append(t_step*step)
-        #if step == 3:
-           # t_step = t_step*2
+        #time.append(step*t_step)
         if step == 0:
             velocity_val = velocity_init
             position_val = position_init
-            
+
             position_array.append(position_val)
             velocity_array.append(velocity_val)
 
-            signal = b.subtract(b.multiply(-2,omega,zeta,velocity_val), b.multiply(b.square(omega),position_val))
+            signal = b.subtract(b.multiply(-2, omega, zeta, velocity_val), b.multiply(b.square(omega), position_val))
             signal_array.append(signal)
-
-            #t_step = t_step/2
             time.append(0)
         elif step == 1:
-            #time.append(time[-1]+t_step)
-            
+            time_results = b.time_handle(step, t_step, time, i)
+            time = time_results[0]
+            t_step = time_results[1]
+
             position_val = .999988
             velocity_val = -.00499898
-            
-            signal = b.subtract(b.multiply(-2,omega,zeta,velocity_val), b.multiply(b.square(omega),position_val))
+
+            signal = b.subtract(b.multiply(-2, omega, zeta, velocity_val), b.multiply(b.square(omega), position_val))
             signal_array.append(signal)
-            
-            integrate_result_vel = b.integrate(step, t_step, signal_array, velocity_val, i, velocity_init, time)
-            velocity_val = integrate_result_vel[0]
-            time = integrate_result_vel[1]
+
+            velocity_val = b.integrate(step, t_step, signal_array, velocity_val, i, velocity_init)
             velocity_array.append(velocity_val)
-            
-            integrate_result_pos = b.integrate(step, t_step, velocity_array, position_val, i, position_init, time)
-            position_val = integrate_result_pos[0]
-            time = integrate_result_vel[1]
+
+            position_val = b.integrate(step, t_step, velocity_array, position_val, i, position_init)
             position_array.append(position_val)
         else:
-            #time.append(time[-1]+t_step)
-            
-            signal = b.subtract(b.multiply(-2,omega,zeta,velocity_val), b.multiply(b.square(omega),position_val))
+            time_results = b.time_handle(step, t_step, time, i)
+            time = time_results[0]
+            t_step = time_results[1]
+
+            signal = b.subtract(b.multiply(-2, omega, zeta, velocity_val), b.multiply(b.square(omega), position_val))
             signal_array.append(signal)
-            
-            integrate_result_vel = b.integrate(step, t_step, signal_array, velocity_val, i, velocity_init, time)
-            velocity_val = integrate_result_vel[0]
-            time = integrate_result_vel[1]
+
+            velocity_val = b.integrate(step, t_step, signal_array, velocity_val, i, velocity_init)
             velocity_array.append(velocity_val)
-            
-            integrate_result_pos = b.integrate(step, t_step, velocity_array, position_val, i, position_init, time)
-            position_val = integrate_result_pos[0]
-            time = integrate_result_pos[1]
+
+            position_val = b.integrate(step, t_step, velocity_array, position_val, i, position_init)
             position_array.append(position_val)
-            
-        step+=1
+
+        step += 1
     results[i] = position_array
     times[i] = time
-
 actual = []
 
 for i in times:
@@ -112,7 +101,7 @@ for i in results.values():
 
 print(list(zip(results.keys(), avg_error)))
 
-#print(tabulate(zip(time, signal_array, results['spec'], actual, errors[0]), headers=['Time', 'signal', 'integrate()', 'actual', 'error']))
+#print(tabulate(zip(time, signal_array, results['trap'], actual, errors[0]), headers=['Time', 'signal', 'integrate()', 'actual', 'error']))
 
-plt.plot(times['simp38'], errors[0])#, time, errors[2], 'k', time, errors[3], 'c', time, errors[4], 'y'
+plt.plot(times['simp13'], errors[0], 'k', times['trap'], errors[1], 'r', times['timevary'], errors[2], 'c')#, time, errors[3], 'y'
 plt.show()
